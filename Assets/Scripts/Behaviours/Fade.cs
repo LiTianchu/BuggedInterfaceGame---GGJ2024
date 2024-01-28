faf2cada42c08cda37ddded1b2f7b88e70db97c0
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2024-01-28 16:06:56 +0800

    fade

---
+++ Assets/Scripts/Behaviours/Fade.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class Fade : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeDuration = 2.0f;
+    [SerializeField]
+    private FadeMode fadeMode = FadeMode.In;
+    [SerializeField]
+    private bool fadeOnStart = true;
+    private Vector2 _originalPos;
+    private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _originalPos = _rectTransform.anchoredPosition;
+
+        if(!fadeOnStart)
+        {
+            return;
+        }
+        StartFade();
+
+    }
+    public void StartFade(){
+        if(fadeMode == FadeMode.In)
+        {
+           UIManager.Instance.UIFadeIn(_canvasGroup, fadeDuration, _originalPos, _originalPos);
+        }else{
+            UIManager.Instance.UIFadeOut(_canvasGroup, fadeDuration, _originalPos, _originalPos);
+        }
+    }
+    
+
+    public void SetFadeTime(float time)
+    {
+        fadeDuration = time;
+    }
+
+    private enum FadeMode
+    {
+        In,
+        Out
+    }
+
+
+}
