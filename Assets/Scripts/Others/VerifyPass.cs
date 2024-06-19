30fc7be31cbcb48aa98b3b4ae82da924c91377a9
LongMingWei <longmw2001@gmail.com>
2024-06-19 23:17:14 +0800

    A

--- Assets/Scripts/Others/VerifyPass.cs
+++ Assets/Scripts/Others/VerifyPass.cs
@@ -7,22 +7,31 @@ using UnityEngine.SceneManagement;
 public class VerifyPass : MonoBehaviour
 {
     [SerializeField] InputField inputField;
+    [SerializeField] bool actual;
     [SerializeField] Text resultText;
+    [SerializeField] Text hintText;
     public Popup popup;
     
     public void ValidateInput()
     {
-        string password = inputField.text;
-        if (password == "PASSWORD")
+        if (actual)
         {
-            inputField.text = "";
-            resultText.text = "";
-            popup.HidePopup();
+            string password = inputField.text;
+            if (password == "PASSWORD" || password == "password")
+            {
+                inputField.text = "";
+                resultText.text = "";
+                popup.HidePopup();
+            }
+            else
+            {
+                resultText.text = "Instructions not followed. Request rejected :D";
+                resultText.color = Color.red;
+            }
         }
         else
         {
-            resultText.text = "Wrong PASSWORD";
-            resultText.color = Color.red;
+            hintText.text = "not this one";
         }
     }
 }
