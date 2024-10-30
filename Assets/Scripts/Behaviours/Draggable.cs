b0fc77bda4269d12fa01175c6d1c15c89befc382
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2024-10-30 23:06:43 +0800

    fix hidden button and window out of bound

--- Assets/Scripts/Behaviours/Draggable.cs
+++ Assets/Scripts/Behaviours/Draggable.cs
@@ -12,10 +12,14 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     [Tooltip("The canvas that this UI component belongs to")]
     private Canvas canvas;
     private RectTransform _rectTransform;
+    private Camera _mainCamera;
+    private RectTransform _canvasRect;
     //private Transform _parent;
     private void Awake()
     {
+        _mainCamera = Camera.main;
         _rectTransform = GetComponent<RectTransform>();
+        _canvasRect = canvas.GetComponent<RectTransform>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -26,7 +30,25 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
 
     public void OnDrag(PointerEventData eventData)
     {
-        _rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        // check if pointer is in the viewport
+        if (RectTransformUtility.RectangleContainsScreenPoint(_canvasRect, Input.mousePosition, _mainCamera))
+            _rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        else
+        {
+            // if pointer is not in the viewport, animate the object to the nearest edge
+            Vector2 pos = _rectTransform.anchoredPosition;
+            Vector2 canvasSize = _canvasRect.sizeDelta;
+            Vector2 newPos = new(pos.x, pos.y);
+            if (pos.x <  - canvasSize.x / 2) // if object at left
+                newPos.x = 0;
+            if (pos.x > canvasSize.x / 2) // if object at right
+                newPos.x = 0;
+            if (pos.y < -canvasSize.y / 2) // if object at bottom
+                newPos.y = 0;
+            if (pos.y > canvasSize.y / 2) // if object at top
+                newPos.y = 0;
+            _rectTransform.DOAnchorPos(newPos, 0.5f);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -38,7 +60,7 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     public void OnPointerDown(PointerEventData eventData)
     {
         //set the order in the hierarchy to the highest
-        transform.SetSiblingIndex(transform.parent.childCount-1);
+        transform.SetSiblingIndex(transform.parent.childCount - 1);
     }
 
     public void SetCanvas(Canvas canvas)
