c07f5ce57287e3c26d848b1b83e00c34edd2474b
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2025-05-27 22:34:40 +0800

    shatter anim for loading bios

--- Assets/Scripts/LevelScripts/LevelHubManager.cs
+++ Assets/Scripts/LevelScripts/LevelHubManager.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class LevelHubManager : Singleton<LevelHubManager>
 {
     [SerializeField] private List<GameObject> levels;
 
-    [SerializeField] private List<GameObject> crumbleObjects;
+    [SerializeField] private List<CrumbleObject> crumbleObjects;
 
+    private int _numOfCrumbleObjects;
+    public event System.Action OnStartLoadingBios;
+
+    private void Start()
+    {
+        _numOfCrumbleObjects = crumbleObjects.Count;
+    }
+
+    private void Update()
+    {
+        if(Keyboard.current.oKey.wasPressedThisFrame)
+        {
+            StartCoroutine(LoadBios());
+        }
+    }
 
     public void ShowLevelHubScreen()
     {
@@ -46,15 +64,23 @@ public class LevelHubManager : Singleton<LevelHubManager>
         }
     }
 
-    public void LoadBios()
+    public IEnumerator LoadBios()
     {
-        for (int i = 0; i < crumbleObjects.Count; i++)
+        OnStartLoadingBios?.Invoke();
+
+        foreach (CrumbleObject crumbleObject in crumbleObjects)
         {
-            
+            crumbleObject.Crumble();
+
+            yield return new WaitForSeconds(Random.Range(0.01f, 0.03f));
         }
+
+        yield return new WaitForSeconds(1.75f);
+        Debug.Log("Loading BIOS...");
+        SceneManager.LoadScene("BIOS", LoadSceneMode.Single);
     }
 
-    
+
 
 
 }
