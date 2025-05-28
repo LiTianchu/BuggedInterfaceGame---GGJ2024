14b4effea4949ef6cdbe6a95de527d24a56ac387
Tianchu_Laptop <tianchu0522@gmail.com>
2025-05-28 19:59:16 +0800

    birthday puzzle complete

--- Assets/Scripts/LevelScripts/Steam/SteamStoreManager.cs
+++ Assets/Scripts/LevelScripts/Steam/SteamStoreManager.cs
@@ -1,19 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SteamStoreManager : MonoBehaviour
 {
-    [Header("Store A Puzzle")]
+    [Header("Store A QR Puzzle")]
     [SerializeField] private CrumblingPieces storeAPuzzle;
     [SerializeField] private StorePuzzleWinScreen storeAPuzzleWinScreen;
+
+    [Header("Store B Birthday Puzzle")]
+    [SerializeField] private RectTransform storeBPuzzle;
+    [SerializeField] private TMP_Text storeBPuzzleNumText1;
+    [SerializeField] private TMP_Text storeBPuzzleNumText2;
+    [SerializeField] private TMP_Text storeBPuzzleNumText3;
+    [SerializeField] private TMP_Text storeBPuzzleNumText4;
+    [SerializeField] private FadingText wrongAgeText;
+    [SerializeField] private Button continueButton;
+    [SerializeField] private string correctBirthday = "2000";
+    [SerializeField] private StorePuzzleWinScreen storeBPuzzleWinScreen;
+
     // Start is called before the first frame update
     void Start()
     {
         storeAPuzzle.OnPuzzlePieceRight += HandleStoreAPuzzleWin;
+        continueButton.onClick.AddListener(CheckBirthday);
     }
-    
+
     void OnDestroy()
     {
         storeAPuzzle.OnPuzzlePieceRight -= HandleStoreAPuzzleWin;
@@ -24,6 +39,30 @@ public class SteamStoreManager : MonoBehaviour
         storeAPuzzleWinScreen.gameObject.SetActive(true);
         storeAPuzzle.gameObject.SetActive(false);
         storeAPuzzle.OnPuzzlePieceRight -= HandleStoreAPuzzleWin;
+    }
+
+    private void CheckBirthday()
+    {
+        string inputBirthday = storeBPuzzleNumText1.text + storeBPuzzleNumText2.text + storeBPuzzleNumText3.text + storeBPuzzleNumText4.text;
+        if (inputBirthday.Equals(correctBirthday))
+        {
+            Debug.Log("Correct birthday entered: " + inputBirthday);
+            wrongAgeText.gameObject.SetActive(false);
+            HandleStoreBPuzzleWin();
+        }
+        else
+        {
+            // Incorrect birthday, show error message
+            Debug.Log("Incorrect birthday entered: " + inputBirthday);
+            wrongAgeText.gameObject.SetActive(true);
+            wrongAgeText.ResetFadingText();
+        }
+    }
+    
+    private void HandleStoreBPuzzleWin()
+    {
+        storeBPuzzleWinScreen.gameObject.SetActive(true);
+        storeBPuzzle.gameObject.SetActive(false);
     }
 
  
