d2d1e592c382b22abfd064651602b67fa919c01b
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2025-06-23 18:41:04 +0800

    integrating paint boss

--- Assets/Scripts/LevelScripts/Paint/PaintSpawn.cs
+++ Assets/Scripts/LevelScripts/Paint/PaintSpawn.cs
@@ -7,16 +7,18 @@ public class PaintSpawn : MonoBehaviour
     public GameObject stickmanPrefab; // Reference to the Stickman prefab
     public Canvas mainCanvas; // Reference to the main canvas
 
+    private Stickman _spawnedStickman; // Reference to the spawned Stickman
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // This method is called when the object becomes enabled and active
@@ -25,17 +27,23 @@ public class PaintSpawn : MonoBehaviour
         SpawnStickman();
     }
 
+    void OnDisable()
+    {
+        if (_spawnedStickman != null)
+        {
+            Destroy(_spawnedStickman.gameObject);
+            _spawnedStickman = null;
+        }
+    }
+
     // Method to spawn the Stickman object
     void SpawnStickman()
     {
-        if (stickmanPrefab != null && mainCanvas != null)
+        if (_spawnedStickman == null && stickmanPrefab != null && mainCanvas != null)
         {
-            GameObject stickman = Instantiate(stickmanPrefab, mainCanvas.transform);
+            GameObject stickman = Instantiate(stickmanPrefab, transform);
             stickman.transform.localPosition = Vector3.zero; // Adjust position as needed
-        }
-        else
-        {
-            Debug.LogError("Stickman prefab or main canvas is not assigned.");
+            _spawnedStickman = stickman.GetComponent<Stickman>();
         }
     }
 }
