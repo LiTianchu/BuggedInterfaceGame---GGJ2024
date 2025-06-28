4d0fc9dcdb4ab4b14641c239bf444f2ceb698430
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2025-06-28 13:45:21 +0800

    modifying dialogue framework

---
+++ Assets/Scripts/Behaviours/UI/PopupUISubtitlePanel.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class PopupUISubtitlePanel : StandardUISubtitlePanel
+{
+    
+}
