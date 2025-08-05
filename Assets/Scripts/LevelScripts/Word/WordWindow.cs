11e836fa6a87bb9669ab0e1e7e8616517fa6b933
LiTianchu <52227175+LiTianchu@users.noreply.github.com>
2025-08-05 19:12:53 +0800

    word level start

--- Assets/Scripts/LevelScripts/Word/WordWindow.cs
+++ Assets/Scripts/LevelScripts/Word/WordWindow.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 [RequireComponent(typeof(Draggable))]
@@ -9,10 +10,14 @@ public class WordWindow : MonoBehaviour
     [SerializeField] private Collectible collectiblePrefab;
     [SerializeField] private RectTransform collectibleSpawnParent;
     [SerializeField] private DoodleController doodleController;
+    [SerializeField] private WordCage wordCage; // Reference to the WordCage component
+    [SerializeField] private GameObject level;
     [SerializeField] private WordLevelGoal wordLevelGoal; // Reference to the WordLevelGoal component
+    [SerializeField] private CameraFollower levelCamera; // Reference to the CameraFollower component
     private Draggable _draggable; // Reference to the Draggable component
     private RectTransform _rectTransform; // Reference to the RectTransform component
-
+    private float _forceMultiplier = 0.05f; // Multiplier for the force applied based on drag delta
+    private bool _levelStarted = false; // Flag to check if the level has started
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +28,10 @@ public class WordWindow : MonoBehaviour
             _draggable.OnDragUpdate += HandleDragUpdate;
         }
         wordLevelGoal.OnGoalCollected += HanleGoalCollected; // Subscribe to the goal collected event
+        wordCage.OnCageBroken += HandleCageBroken; // Subscribe to the cage broken event
+        doodleController.OnLandOnGround += HandleLevelStart;
     }
+
 
 
     void OnDestroy()
@@ -32,19 +40,41 @@ public class WordWindow : MonoBehaviour
         {
             _draggable.OnDragUpdate -= HandleDragUpdate; // Unsubscribe from the event to avoid memory leaks
         }
+        wordLevelGoal.OnGoalCollected -= HanleGoalCollected; // Unsubscribe from the goal collected event
+        wordCage.OnCageBroken -= HandleCageBroken; // Unsubscribe from the
+        doodleController.OnLandOnGround -= HandleLevelStart; // Unsubscribe from the level start event
+    }
+
+    private void HandleCageBroken()
+    {
+        _forceMultiplier = 0.02f;
+        doodleController.UpDownMovement = false; // Disable up-down movement
+        doodleController.LeftRightMovement = true; // Enable left-right movement
+        levelCamera.Following = true;
     }
 
     private void HandleDragUpdate(Vector2 dragDelta)
     {
         if (dragDelta.x < 0)
         {
-            doodleController.PushLeft(Mathf.Abs(dragDelta.x) * 0.02f);
+            doodleController.PushLeft(Mathf.Abs(dragDelta.x) * _forceMultiplier);
             //doodleController.MoveLeft();
         }
         else if (dragDelta.x > 0)
         {
-            doodleController.PushRight(Mathf.Abs(dragDelta.x) * 0.02f);
+            doodleController.PushRight(Mathf.Abs(dragDelta.x) * _forceMultiplier);
             //doodleController.MoveRight();
+        }
+
+        if (dragDelta.y < 0)
+        {
+            doodleController.PushDown(Mathf.Abs(dragDelta.y) * _forceMultiplier);
+            //doodleController.MoveDown();
+        }
+        else if (dragDelta.y > 0)
+        {
+            doodleController.PushUp(Mathf.Abs(dragDelta.y) * _forceMultiplier);
+            //doodleController.MoveUp();
         }
     }
 
@@ -53,9 +83,17 @@ public class WordWindow : MonoBehaviour
         wordLevelGoal.OnGoalCollected -= HanleGoalCollected; // Unsubscribe to avoid memory leaks
         float spawnAnchorX = Mathf.Clamp(_rectTransform.anchoredPosition.x, -350f, 350f); // Clamp the spawn position within the level bounds
         float spawnAnchorY = _rectTransform.anchoredPosition.y + 500f;
-        
+
         Collectible newCollectible = Instantiate(collectiblePrefab, collectibleSpawnParent);
         newCollectible.GetComponent<RectTransform>().anchoredPosition = new Vector2(spawnAnchorX, spawnAnchorY);
+    }
+
+    private void HandleLevelStart()
+    {
+        if (_levelStarted) { return; }
+
+        _levelStarted = true; // Set the level started flag to true
+        level.transform.DOLocalMoveX(0f, 1f).SetEase(Ease.OutBack); // Move the level to its starting position
     }
 
 }
---
+++ Assets/WordCage.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WordCage : MonoBehaviour
+{
+    [SerializeField] private int maxHp = 20;
+    [SerializeField] private LayerMask worldLevelPlayerLayer;
+    [SerializeField] private ShakeAnim shakeAnim;
+    [SerializeField] private GameObject cageParts;
+    [SerializeField] private List<CagePartBatch> cagePartsBreakableBatches;
+
+    private EdgeCollider2D _edgeCollider;
+    private int _currentHp;
+    public int CurrentHp { get => _currentHp; }
+    private List<Rigidbody2D> _cageParts = new List<Rigidbody2D>();
+
+    public event System.Action OnCageBroken;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in cageParts.transform)
+        {
+            Rigidbody2D rb = child.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                _cageParts.Add(rb);
+            }
+        }
+        _edgeCollider = GetComponent<EdgeCollider2D>();
+        _currentHp = maxHp; // Initialize current HP to max HP
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHp -= damage;
+
+        CagePartBatch batchToRemove = null;
+
+        foreach (CagePartBatch batch in cagePartsBreakableBatches)
+        {
+            if ((_currentHp / (float)maxHp) <= batch.hpThreshold)
+            {
+                foreach (Rigidbody2D rb in batch.cageParts)
+                {
+                    if (_cageParts.Contains(rb)) // remove if it is in the list to prevent error
+                    {
+                        _cageParts.Remove(rb);
+                    }
+
+                    BreakPart(rb); // Break the part
+                }
+                batchToRemove = batch;
+                break;
+            }
+        }
+
+        if (batchToRemove != null)
+        {
+            cagePartsBreakableBatches.Remove(batchToRemove);
+        }
+
+        if (_currentHp <= 0)
+        {
+            OnCageBroken?.Invoke();
+            BreakCage();
+        }
+    }
+
+    public void BreakCage()
+    {
+        foreach (Rigidbody2D rb in _cageParts)
+        {
+            BreakPart(rb);
+        }
+        _edgeCollider.enabled = false; // Disable the edge collider
+    }
+
+    public void BreakPart(Rigidbody2D rb)
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic; // Change to dynamic to allow physics interaction
+        rb.isKinematic = false; // Ensure the Rigidbody is not kinematic
+        rb.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * 5f, ForceMode2D.Impulse);
+        rb.GetComponent<Collider2D>().enabled = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((1 << collision.gameObject.layer & worldLevelPlayerLayer) != 0)
+        {
+            // Handle collision with player
+            TakeDamage(1);
+            shakeAnim.PlayOneShot(0.1f);
+        }
+    }
+
+    [System.Serializable]
+    public class CagePartBatch
+    {
+        public List<Rigidbody2D> cageParts;
+        public float hpThreshold;
+
+        public CagePartBatch(List<Rigidbody2D> cageParts, float hpThreshold)
+        {
+            this.cageParts = cageParts;
+            this.hpThreshold = hpThreshold;
+        }
+    }
+}
